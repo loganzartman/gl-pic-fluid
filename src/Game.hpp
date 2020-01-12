8b36f779a6165ec4eb98ea18da77811ba2bf999e
Logan Zartman <logan.zartman@utexas.edu>
2020-01-12 17:20:56 -0600

    hopefully successfully create VAO

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -7,10 +8,64 @@ public:
     GLFWwindow* window;
     glm::dvec2 mouse_pos;
 
+    GLuint rect_vao;
+
     Game(GLFWwindow* window) : window(window) {}
 
     void init() {
+        create_rect_vao();
+    }
 
+    void create_rect_vao() {
+        // create VAO, which encapsulates the vertex buffer and its layout
+        // https://www.khronos.org/opengl/wiki/Vertex_Specification#Vertex_Array_Object
+        glGenVertexArrays(1, &rect_vao);
+        glBindVertexArray(rect_vao);
+
+        GLuint rect_vbo;
+        glGenBuffers(1, &rect_vbo);
+
+        glEnableVertexAttribArray(/* attribute index */ 0);
+
+        // binds the VBO to a VAO attribute
+        // https://www.khronos.org/opengl/wiki/Vertex_Specification#Separate_attribute_format
+        glBindVertexBuffer(
+            0,                // binding (attribute) index
+            rect_vbo,         // buffer
+            0,                // offset of first element
+            sizeof(glm::vec2) // stride
+        );
+        // notice how we could bind different slices of the same buffer to different attributes using offset and stride.
+        
+        glVertexAttribFormat(
+            0,                   // attribute index
+            glm::vec2::length(), // size (number of components)
+            GL_FLOAT,            // type of components
+            GL_FALSE,            // normalize integer type?
+            0                    // "added to the buffer binding's offset to get the offset for this attribute"
+        );
+
+        // could use glVertexBindingDivisor() to do instancing
+
+        constexpr std::array<glm::vec2, 4> vbo_data{
+            glm::vec2(0, 0),
+            glm::vec2(1, 0),
+            glm::vec2(1, 1),
+            glm::vec2(0, 1)
+        };
+
+        // load data into the VBO
+        // https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferData.xhtml
+        glBindBuffer(GL_ARRAY_BUFFER, rect_vbo);
+        glBufferData(
+            GL_ARRAY_BUFFER,                     // target
+            vbo_data.size() * sizeof(glm::vec2), // size in bytes of data
+            vbo_data.data(),                     // pointer to data
+            GL_STATIC_DRAW                       // usage (GL_STATIC_DRAW - unlikely to be updated)
+        );
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind
+        glBindVertexArray(0); // unbind
     }
 
     void update() {
