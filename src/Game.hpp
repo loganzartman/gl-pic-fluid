9febdf9ed29caf639dc888b44d870b2cc6c3b5a5
Logan Zartman <logan.zartman@utexas.edu>
2020-01-12 01:31:07 -0600

    tabs -> spaces

--- src/Game.hpp
+++ src/Game.hpp
@@ -4,21 +4,21 @@
 
 class Game {
 public:
-	GLFWwindow* window;
-	glm::dvec2 mouse_pos;
+    GLFWwindow* window;
+    glm::dvec2 mouse_pos;
 
-	Game(GLFWwindow* window) : window(window) {}
+    Game(GLFWwindow* window) : window(window) {}
 
-	void init() {
+    void init() {
 
-	}
+    }
 
-	void update() {
-		int window_w, window_h;
-		glfwGetFramebufferSize(window, &window_w, &window_h);
+    void update() {
+        int window_w, window_h;
+        glfwGetFramebufferSize(window, &window_w, &window_h);
 
-		glViewport(0, 0, window_w, window_h);
-		glClearColor(0.25, 0.45, 0.75, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	}
+        glViewport(0, 0, window_w, window_h);
+        glClearColor(0.25, 0.45, 0.75, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,31 @@
 * Called in the event that any GLFW function fails.
 */
 void glfw_error_callback(int error, const char* description) {
-	std::stringstream str;
-	str << "GLFW error " << error << ": " << description;
-	throw std::runtime_error(str.str());
+    std::stringstream str;
+    str << "GLFW error " << error << ": " << description;
+    throw std::runtime_error(str.str());
 }
 
 void GLAPIENTRY
 MessageCallback(GLenum source,
-	GLenum type,
-	GLuint id,
-	GLenum severity,
-	GLsizei length,
-	const GLchar* message,
-	const void* userParam)
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam)
 {
-	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
-		type, severity, message);
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+        type, severity, message);
 }
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 }
 
 void MousePosCallback(GLFWwindow* window, double mouse_x, double mouse_y) {
-	Game* game = static_cast<Game*>(glfwGetWindowUserPointer(window));
-	game->mouse_pos = glm::dvec2(mouse_x, mouse_y);
+    Game* game = static_cast<Game*>(glfwGetWindowUserPointer(window));
+    game->mouse_pos = glm::dvec2(mouse_x, mouse_y);
 }
 
 void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
@@ -44,42 +44,42 @@ void FramebufferSizeCallback(GLFWwindow* window, int w, int h) {
 }
 
 int main() {
-	// setup glfw
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit()) { throw std::runtime_error("glfwInit failed"); }
+    // setup glfw
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) { throw std::runtime_error("glfwInit failed"); }
 
-	// create window and GL context
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Sand in my Fans", NULL, NULL);
-	if (!window) { throw std::runtime_error("glfwCreateWindow failed"); }
-	glfwMakeContextCurrent(window);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	std::cout << "** GL Version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    // create window and GL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Sand in my Fans", NULL, NULL);
+    if (!window) { throw std::runtime_error("glfwCreateWindow failed"); }
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    std::cout << "** GL Version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
-	glfwSwapInterval(1); // enable vsync (0 to disable)
+    glfwSwapInterval(1); // enable vsync (0 to disable)
 
-	// Callback functions
-	glfwSetKeyCallback(window, KeyCallback);
-	glfwSetCursorPosCallback(window, MousePosCallback);
-	glfwSetMouseButtonCallback(window, MouseButtonCallback);
-	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+    // Callback functions
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetCursorPosCallback(window, MousePosCallback);
+    glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
-	// Enable GL error/debug reporting
-	glEnable(GL_DEBUG_OUTPUT);
-	glDebugMessageCallback(MessageCallback, 0);
+    // Enable GL error/debug reporting
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
 
-	Game game(window);
-	glfwSetWindowUserPointer(window, &game);
+    Game game(window);
+    glfwSetWindowUserPointer(window, &game);
 
-	// main loop
-	game.init();
-	while (!glfwWindowShouldClose(window)) {
-		game.update();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    // main loop
+    game.init();
+    while (!glfwWindowShouldClose(window)) {
+        game.update();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
