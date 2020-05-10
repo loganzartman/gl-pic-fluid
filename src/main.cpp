7589622810974e4138ded49aee58b939482507b7
Logan Zartman <logan.zartman@utexas.edu>
2020-05-10 17:03:19 -0500

    ignore shader compile errors in MessageCallback to get better error messages

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@ MessageCallback(GLenum source,
     if (severity == GL_DEBUG_SEVERITY_NOTIFICATION) {
         return;
     }
+    if (type == 0x824c) {
+        // hack to get actual compile errors for shaders (program objects check their own errors)
+        return;
+    }
     fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
         (type == GL_DEBUG_TYPE_ERROR ? "ERROR!" : ""),
         type, severity, message);
