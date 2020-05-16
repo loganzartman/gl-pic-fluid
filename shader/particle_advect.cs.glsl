af03ed2825c1e6704e91091e10ee041d312ece2c
Logan Zartman <logan.zartman@utexas.edu>
2020-05-16 17:06:11 -0500

    add epsilon to particle bounds clamping

--- shader/particle_advect.cs.glsl
+++ shader/particle_advect.cs.glsl
@@ -1,10 +1,11 @@
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
 uniform float dt;
+const float epsilon = 0.00001;
 
 void main() {
     uint index = gl_WorkGroupID.x;
     // TODO: don't use explicit Euler integration
     particle[index].pos += particle[index].vel * dt;
-    particle[index].pos = clamp(particle[index].pos, bounds_min, bounds_max);
+    particle[index].pos = clamp(particle[index].pos, bounds_min, bounds_max - vec3(epsilon));
 }
