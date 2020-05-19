c36c9d4631e5fae5bb82ff5a19fdf32b81521bda
Logan Zartman <logan.zartman@utexas.edu>
2020-05-18 22:26:27 -0500

    bugfix SSF (WIP)

--- shader/ssf_shade.fs.glsl
+++ shader/ssf_shade.fs.glsl
@@ -19,28 +19,29 @@ void main() {
     // sample eye-space positions of current pixel and neighbors
     vec4 eye_pos = texture(sphere_pos_tex, uv);
     vec4 eye_pos_px = texture(sphere_pos_tex, uv + vec2(1.0 / resolution.x, 0));
-    vec4 eye_pos_py = texture(sphere_pos_tex, uv + vec2(0, -1.0 / resolution.y));
-    vec4 eye_pos_nx = texture(sphere_pos_tex, uv + vec2(1.0 / resolution.x, 0));
+    vec4 eye_pos_py = texture(sphere_pos_tex, uv + vec2(0, 1.0 / resolution.y));
+    vec4 eye_pos_nx = texture(sphere_pos_tex, uv + vec2(-1.0 / resolution.x, 0));
     vec4 eye_pos_ny = texture(sphere_pos_tex, uv + vec2(0, -1.0 / resolution.y));
 
     // choose smaller difference to handle edges
     vec3 ab, ac;
-    if (abs(eye_pos_px.z - eye_pos.z) < abs(eye_pos.z - eye_pos_nx.z)) {
+    if (abs(eye_pos_px.z - eye_pos.z) < abs(eye_pos_nx.z - eye_pos.z)) {
         ab = eye_pos_px.xyz - eye_pos.xyz;
     } else {
         ab = eye_pos.xyz - eye_pos_nx.xyz;
     }
-    if (abs(eye_pos_py.z - eye_pos.z) < abs(eye_pos.z - eye_pos_ny.z)) {
+    if (abs(eye_pos_py.z - eye_pos.z) < abs(eye_pos_ny.z - eye_pos.z)) {
         ac = eye_pos_py.xyz - eye_pos.xyz;
     } else {
-        ac = eye_pos.xyz - eye_pos_py.xyz;
+        ac = eye_pos.xyz - eye_pos_ny.xyz;
     }
 
     // compute normal
-    vec3 eye_normal = normalize(cross(ab, ac));
-    vec3 normal = (inv_view * vec4(eye_normal, 0)).xyz;
+    vec3 eye_normal = cross(ab, ac);
+    vec3 normal = normalize((inv_view * vec4(eye_normal, 0)).xyz);
 
-    vec3 phong_color = shade(eye, normalize(look), normal, vec3(0.1), vec3(0.5), vec3(0.1), 64);
-    frag_color = vec4(normal, color.a);
+    vec3 world_pos = (inv_view * eye_pos).xyz;
+    vec3 phong_color = shade(world_pos, normalize(look), normal, vec3(0.1), vec3(0.1, 0.2, 0.5), vec3(0.5), 64);
+    frag_color = vec4(phong_color, color.a);
     gl_FragDepth = depth;
 }
